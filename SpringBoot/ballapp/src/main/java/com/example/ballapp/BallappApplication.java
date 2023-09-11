e93873489e0e4ec7585468fcf459d733e92eb285
nirajsakhare0033 <kangthecon0111@gmail.com>
2023-09-11 13:38:48 +0530

    angular and spring app created

---
+++ SpringBoot/ballapp/src/main/java/com/example/ballapp/BallappApplication.java
@@ -0,0 +1,13 @@
+package com.example.ballapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallappApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/ballapp/src/test/java/com/example/ballapp/BallappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ballapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
